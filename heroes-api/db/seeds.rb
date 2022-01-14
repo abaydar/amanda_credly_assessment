b3f218fe755a7da0c09e78aebf8824a806c227a2
Amanda <amandabaydar@gmail.com>
2022-01-14 15:43:37 -0500

    create user and badges, seed user data

--- heroes-api/db/seeds.rb
+++ heroes-api/db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# response = RestClient.get("https://randomuser.me/api/?results=100")
+
+# user_array = JSON.parse(response.body)
+# user_array["results"].each do |user|
+#     User.create(first_name: user["name"]["first"], last_name: user["name"]["last"], age: user["dob"]["age"])
+# end
